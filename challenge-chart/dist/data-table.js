0797bc145afa1a986bf7dd0b03f48e828eba3b35
Anisha <anisha.0612@live.com>
2021-05-31 18:51:09 -0500

    populate table and chart

--- challenge-chart/dist/data-table.js
+++ challenge-chart/dist/data-table.js
@@ -6,30 +6,70 @@ import { ChallengeDataService } from "../../ChallengeDataService.js";
 const buttons = document.querySelectorAll(".btn-type");
 
 const resetButton = document.getElementById("reset");
-
 const tableContainer = document.querySelector(".data-table");
+
+const reset = () => {
+  history.go();
+};
 
 // Event listener for Reset Button
 
 resetButton.addEventListener("click", () => {
-  history.go();
+  reset();
 });
 
-const createTableHeader = () => {
-  const values = ["x", "y"];
+// function to create and populate table header
+const createTableHeader = (values) => {
   let tHead = tableContainer.createTHead();
   let row = tHead.insertRow();
   values.forEach((value) => {
     let th = document.createElement("th");
     let text = document.createTextNode(value);
+    th.classList.add("p-2", "h4");
     th.appendChild(text);
     row.appendChild(th);
   });
 };
 
+// function to populate table content
+const createTable = (columnX, columnY) => {
+  for (let i = 0; i < columnX.length; i++) {
+    let row = tableContainer.insertRow();
+    let cellX = row.insertCell();
+    cellX.classList.add("p-2");
+    let textX = document.createTextNode(columnX[i]);
+    cellX.appendChild(textX);
+    let cellY = row.insertCell();
+    cellY.classList.add("p-2");
+    let textY = document.createTextNode(columnY[i]);
+    cellY.appendChild(textY);
+  }
+};
+
+// function to create and populate chart
+
+const createChart = (columnX, columnY) => {
+  const chart = document.querySelector("challenge-chart");
+  let chartData = [];
+  for (let i = 0; i < columnX.length; i++) {
+    chartData.push({ x: columnX[i], y: columnY[i] });
+  }
+  chart.data = chartData;
+};
+
+// function to create table content and create chart
 const fetchDataSet = (data) => {
   // console.log(data);
-  createTableHeader();
+  tableContainer.innerHTML = ``;
+  const chartType = document.querySelector(".chart-type");
+  chartType.innerHTML = ``;
+  chartType.innerHTML = `${data._name}`;
+  const headerValues = [data._xColumn._name, data._yColumn._name];
+  const columnX = data._xColumn._values;
+  const columnY = data._yColumn._values;
+  createTableHeader(headerValues);
+  createTable(columnX, columnY);
+  createChart(columnX, columnY);
 };
 
 // Add functionality to User clicked button
